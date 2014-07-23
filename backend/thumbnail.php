96acfc2b80001dcc862ac488b851d99d58c9e1c7
Chaz Schlarp <schlarpc@gmail.com>
2014-07-22 19:29:25 -0500

    Center error text

--- backend/thumbnail.php
+++ backend/thumbnail.php
@@ -8,16 +8,26 @@ if (!check_params(["path", "time"])) invalid_request();
 
 header("Content-type: image/jpeg");
 $thumb = get_thumbnail($_GET["path"], $_GET["time"]);
+
 if ($thumb !== FALSE) {
     echo $thumb;
+
 } else {
     $width = $config['thumbnail_width'];
     $height = $width * 9 / 16;
+    
+    $font_size = 5;
+    $error = 'Thumbnail error';
+    
+    $x = ($width / 2) - (imagefontwidth($font_size) * strlen($error) / 2);
+    $y = ($height / 2) - (imagefontheight($font_size) / 2);
+    
     $im = imagecreatetruecolor($width, $height);
     $fg = imagecolorallocate($im, 255, 255, 255);
     $bg = imagecolorallocate($im, 0, 0, 0);
+
     imagefill($im, 0, 0, $bg);
-    imagestring($im, 2, 0, $height / 2, 'Thumbnail error', $fg);
+    imagestring($im, 5, $x, $y, $error, $fg);
     imagejpeg($im);
     imagedestroy($im);
 }
