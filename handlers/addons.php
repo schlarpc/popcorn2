05d2b05827bbdc06cfdcfdf4a46082711a4d9933
Chaz Schlarp <schlarpc@gmail.com>
2014-07-24 14:24:46 -0500

    Dynamically load addons and routes

--- handlers/addons.php
+++ handlers/addons.php
@@ -2,6 +2,17 @@
 
 class AddonsListHandler extends AuthenticationRequired  {
     function get() {
+        $addon_dir = "handlers/addons/";
+        $addon_list = array();
+        if ($dh = opendir($addon_dir)) {
+            while (($entry = readdir($dh)) !== false) {
+                if ($entry != "." && $entry != ".." && is_dir($addon_dir . $entry)) {
+                    $addon_list[] = "/api/addons/" . $entry;
+                }
+            }
+            closedir($dh);
+        }
         
+        json_response(array("addons" => $addon_list));
     }
 }
--- index.php
+++ index.php
@@ -9,11 +9,27 @@ require("handlers/videos.php");
 require("handlers/stream.php");
 require("handlers/addons.php");
 
+$routes = array(
+    "/"                            => "HomeHandler",
+    "/api"                         => "APIHandler",
+    "/api/videos"                  => "VideosListHandler",
+    "/api/videos/:alpha"           => "VideosInfoHandler",
+    "/api/videos/:alpha/thumbnail" => "VideosThumbnailHandler",
+    "/api/stream"                  => "StreamStatusHandler",
+    "/api/stream/:alpha"           => "StreamCommandHandler",
+    "/api/addons"                  => "AddonsListHandler",
+);
+
+
 $addon_dir = "handlers/addons/";
+$addon_list = array();
 if ($dh = opendir($addon_dir)) {
     while (($entry = readdir($dh)) !== false) {
         if ($entry != "." && $entry != ".." && is_dir($addon_dir . $entry)) {
-            echo "$entry\n";
+            $addon_list[] = $entry;
+            require($addon_dir . $entry . "/addon.php");
+            $routes = array_merge($routes, $addon_routes);
+            unset($addon_routes);
         }
     }
     closedir($dh);
@@ -24,13 +40,4 @@ ToroHook::add("404",  function() {
     exit;
 });
 
-Toro::serve(array(
-    "/"                            => "HomeHandler",
-    "/api"                         => "APIHandler",
-    "/api/videos"                  => "VideosListHandler",
-    "/api/videos/:alpha"           => "VideosInfoHandler",
-    "/api/videos/:alpha/thumbnail" => "VideosThumbnailHandler",
-    "/api/stream"                  => "StreamStatusHandler",
-    "/api/stream/:alpha"           => "StreamCommandHandler",
-    "/api/addons"                  => "AddonsListHandler",
-));
+Toro::serve($routes);
