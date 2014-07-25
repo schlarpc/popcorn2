e86eec13b5be886a58ac5bae195603928bd5fcbb
Chaz Schlarp <schlarpc@gmail.com>
2014-07-25 11:40:10 -0500

    Double encoding JSON

--- handlers/addons.php
+++ handlers/addons.php
@@ -22,6 +22,6 @@ class AddonsListHandler extends AuthenticationRequired  {
             closedir($dh);
         }
         
-        json_response($dir->toJSON());
+        echo $dir->toJSON();
     }
 }
